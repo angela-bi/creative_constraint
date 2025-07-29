a5cdb5e61e4d4be0a0db97da6b93c3be8f7ab152
angelalbi <angelabi2003@berkeley.edu>
2025-07-29 12:35:00 -0400

    updated script functionality

--- blender/blender_script.py
+++ blender/blender_script.py
@@ -3,9 +3,10 @@ import numpy as np
 import bpy
 
 audio = sd.rec(int(44100), samplerate=44100, channels=1)
+audio = np.array(audio.flatten())
 rms = np.linalg.norm(audio) / np.sqrt(len(audio))
-print("RMS Volume:", rms) # prints audio level every second
+print("audio shape:", audio.shape)
+print("RMS Volume:", rms)
 
-bpy.data.brushes['Tint'].color = (rms, rms, rms)
-print(bpy.data.brushes['Tint'].color) # confirm that color got changed
-
+bpy.data.brushes['Tint'].color = (np.random.choice(audio)*10, np.random.choice(audio)*10, np.random.choice(audio)*10)
+print(bpy.data.brushes['Tint'].color)
